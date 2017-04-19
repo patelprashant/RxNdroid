3ff97a661c7cf7a6bd3bef87debec724b6e1640b
Prashant <prashant.patel77@gmail.com>
2017-04-19 23:01:44 +1000

    Used List view and adapter to show the data

---
+++ app/src/main/java/com/practice/android/rxndroid/model/DataItem.java
@@ -0,0 +1,93 @@
+package com.practice.android.rxndroid.model;
+
+public class DataItem {
+    private String itemId;
+    private String itemName;
+    private String description;
+    private String category;
+    private int sortPosition;
+    private double price;
+    private String image;
+
+    public DataItem() {
+    }
+
+    public DataItem(String itemId, String itemName, String description, String category, int sortPosition, double price, String image) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.description = description;
+        this.category = category;
+        this.sortPosition = sortPosition;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getSortPosition() {
+        return sortPosition;
+    }
+
+    public void setSortPosition(int sortPosition) {
+        this.sortPosition = sortPosition;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "DataItem{" +
+                "itemId='" + itemId + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", sortPosition=" + sortPosition +
+                ", price=" + price +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
