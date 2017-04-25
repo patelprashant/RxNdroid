22c7274d85bda7a417e1b678020465edbecd62af
Prashant <prashant.patel77@gmail.com>
2017-04-25 13:11:37 +1000

    Added SQLite database helper and created database.

--- app/src/main/java/com/practice/android/rxndroid/Sample3Activity.java
+++ app/src/main/java/com/practice/android/rxndroid/Sample3Activity.java
@@ -1,6 +1,8 @@
 package com.practice.android.rxndroid;
 
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
@@ -8,7 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
+import com.practice.android.rxndroid.database.DBHelper;
 import com.practice.android.rxndroid.model.DataItem;
 import com.practice.android.rxndroid.sample.SampleDataProvider;
 
@@ -24,44 +28,49 @@ public class Sample3Activity extends AppCompatActivity {
     RecyclerView menuListView;
     DataItemRvAdapter dataItemRvAdapter;
 
+    SQLiteDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sample3);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_sample3 );
         setupActionBar();
 
-        Collections.sort(dataItemList, new Comparator<DataItem>() {
+        SQLiteOpenHelper dbHelper = new DBHelper( this );
+        database = dbHelper.getWritableDatabase();
+
+        Toast.makeText( this, "Database acquired", Toast.LENGTH_SHORT ).show();
+
+        Collections.sort( dataItemList, new Comparator<DataItem>() {
             @Override
             public int compare(DataItem o1, DataItem o2) {
-                return o1.getItemName().compareTo(o2.getItemName());
+                return o1.getItemName().compareTo( o2.getItemName() );
             }
-        });
+        } );
 
 
-
-        menuListView = (RecyclerView) findViewById(R.id.menu_list_rv);
-        dataItemRvAdapter = new DataItemRvAdapter(this, dataItemList);
+        menuListView = (RecyclerView) findViewById( R.id.menu_list_rv );
+        dataItemRvAdapter = new DataItemRvAdapter( this, dataItemList );
 
         // Read the preferences and set the layout manager
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences( this );
         boolean isGridView = settings.getBoolean( getString( R.string.pref_display_grid ), false );
 
-        if (isGridView){
+        if (isGridView) {
             menuListView.setLayoutManager( new GridLayoutManager( this, 3 ) );
-        }
-        else{
-            menuListView.setLayoutManager(new LinearLayoutManager(this));
+        } else {
+            menuListView.setLayoutManager( new LinearLayoutManager( this ) );
 
         }
 
-        menuListView.setAdapter(dataItemRvAdapter);
+        menuListView.setAdapter( dataItemRvAdapter );
     }
 
     private void setupActionBar() {
         ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null) {
-            actionBar.setTitle(R.string.sample3_title);
+        if (actionBar != null) {
+            actionBar.setTitle( R.string.sample3_title );
         }
     }
 }
