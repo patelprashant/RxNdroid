12b175922c79e862e5a888f030d397ce557caae4
Prashant <prashant.patel77@gmail.com>
2017-05-02 22:04:54 +1000

    Lifecycle of Fragment

--- app/src/main/java/com/practice/android/rxndroid/BlankFragment.java
+++ app/src/main/java/com/practice/android/rxndroid/BlankFragment.java
@@ -1,8 +1,11 @@
 package com.practice.android.rxndroid;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,18 +15,76 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class BlankFragment extends Fragment {
+    public static final String TAG = "LifecycleEvents";
 
 
     public BlankFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach( context );
+        Log.i( TAG, "onAttach: " );
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        Log.i( TAG, "onCreate: " );
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.i( TAG, "onCreateView: " );
+        
         // Inflate the layout for this fragment
         return inflater.inflate( R.layout.fragment_blank, container, false );
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated( savedInstanceState );
+        Log.i( TAG, "onActivityCreated: " );
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i( TAG, "onStart: " );
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i( TAG, "onResume: " );
+    }
+
+
+    //Removing
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i( TAG, "onPause: " );
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i( TAG, "onStop: " );
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i( TAG, "onDestroy: " );
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i( TAG, "onDetach: " );
+    }
 }
