23350bc664e6d5de83d2849134b9163af7815f85
Prashant <prashant.patel77@gmail.com>
2017-05-02 21:41:13 +1000

    Add / Remove Fragment with code

--- app/src/main/java/com/practice/android/rxndroid/Sample5Activity.java
+++ app/src/main/java/com/practice/android/rxndroid/Sample5Activity.java
@@ -2,8 +2,10 @@ package com.practice.android.rxndroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Sample5Activity extends AppCompatActivity {
+    private BlankFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,11 +13,22 @@ public class Sample5Activity extends AppCompatActivity {
         setContentView( R.layout.activity_sample5 );
 
         //Add fragment with code
-        BlankFragment fragment = new BlankFragment();
+        fragment = new BlankFragment();
+    }
 
+    public void addClickHandler(View view) {
         getSupportFragmentManager()
                 .beginTransaction()
                 .add( R.id.fragment_container, fragment )
                 .commit();
+    }
+
+    public void removeClickHandler(View view) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .remove( fragment )
+                    .commit();
+        }
     }
 }
